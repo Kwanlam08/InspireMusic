a5fffede8f532d1e933bd2914dcad1f6a4c2209f
User <user@example.com>
2026-07-04 00:42:03 +0800

    Release 3.0.6 with updated launcher icon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.inspiremusic"
         minSdk = 26
         targetSdk = 34
-        versionCode = 22
-        versionName = "3.0.5"
+        versionCode = 23
+        versionName = "3.0.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
